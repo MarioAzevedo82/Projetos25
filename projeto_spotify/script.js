4786c2de61bc7f9279983623d08e92021414b14c
Mario Azevedo <mariobct@msn.com>
2025-04-04 19:06:30 -0300

    criada a funcao atualizarBarra

--- projeto_spotify/script.js
+++ projeto_spotify/script.js
@@ -2,11 +2,14 @@ document.querySelector('.botao-pause').style.display = 'none';//sumir o botão p
 
 let musica = document.querySelector('audio');
 
+//Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
+musica.addEventListener('timeupdate', atualizarBarra);
 
+//Funções
 function tocarMusica() {
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
@@ -17,4 +20,9 @@ function pausarMusica() {
     musica.pause();
     document.querySelector('.botao-play').style.display = 'block'
     document.querySelector('.botao-pause').style.display = 'none'
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
 }
