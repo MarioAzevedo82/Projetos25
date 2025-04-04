ebbe0b17ecded4002aca6732b444f5b58066b472
Mario Azevedo <mariobct@msn.com>
2025-04-04 18:44:11 -0300

    Adicionado o botao-pause e omitir botao-play

--- projeto_spotify/script.js
+++ projeto_spotify/script.js
@@ -1,7 +1,11 @@
+document.querySelector('.botao-pause').style.display = 'none';//sumir o botão pause enquanto não aperta o play
+
 let musica = document.querySelector('audio');
 
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 function tocarMusica() {
-    musica.play()
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
 }
