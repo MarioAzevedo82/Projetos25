719890ebcd78395b4ff1594fd6218f8d8ac5ae61
Mario Azevedo <mariobct@msn.com>
2025-04-05 21:43:44 -0300

    Adicionados funçao renderizar musica e imagens

--- projeto_spotify/script.js
+++ projeto_spotify/script.js
@@ -1,27 +1,30 @@
 document.querySelector('.botao-pause').style.display = 'none';//sumir o botão pause enquanto não aperta o play
 let musicas = [
     {
-        titulo: 'Guitar Solo', artista: 'Mario A', src: 'musicas/Mickael Jackson - Bad (1).mp3',
+        titulo: 'Guitar Solo', artista: 'M Jackson', src: 'musicas/Mickael Jackson - Bad (1).mp3',
         img: 'imagens/call-me-fred-X6kdEEqxbB4-unsplash.jpg'
     },
     {
         titulo: 'BlackWhite', artista: 'M J', src: 'musicas/Mickael Jackson - Black or white.mp3',
-        img: 'kenny-jWJn5vG4tns-unsplash (1).jpg'
+        img: 'imagens/mickaeljackson.jpg'
     },
     {
         titulo: 'saysaysay', artista: 'M J paul', src: 'musicas/Mickael Jackson & Paul Mc Cartney - Say, Say, Say.mp3',
-        img: 'mathew-browne-kNyvx1Z_gho-unsplash.jpg'
+        img: 'imagens/mjbad.jpg'
     }
 ];
 
 let musica = document.querySelector('audio');
+let indexMusica = 0;
 
-let duracaMusica = document.querySelector('.fim');
+let duracaoMusica = document.querySelector('.fim');
 let imagem = document.querySelector('img');
 let nomeMusica = document.querySelector('.descricao h2');
 let nomeArtista = document.querySelector('.descricao i')
 
-duracaMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+
+
+duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
 
 //Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
@@ -30,8 +33,27 @@ document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
 musica.addEventListener('timeupdate', atualizarBarra);
 
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    renderizarMusica(indexMusica);
+});
 
 //Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
 function tocarMusica() {
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
