70fdb51600d0326075e5a816d37c0f1576654f5b
Mario Azevedo <mariobct@msn.com>
2025-04-07 12:07:28 -0300

    Fazendo o ponteiro dos segundos se mover

---
+++ CLOCK/script.js
@@ -0,0 +1,20 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+const getTime = () => {
+    const date = new Date()
+
+    return {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    }
+    
+}
+
+setInterval(() => {
+    const {seconds} = getTime()
+
+    secondHand.style.transform = `translate(0, -50%) rotate(${seconds}deg)`;
+}, 1000);
