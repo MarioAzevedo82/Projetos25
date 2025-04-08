26745e1de3f4ef0f33bc21afe6b3b291be7ad888
Mario Azevedo <mariobct@msn.com>
2025-04-08 17:40:51 -0300

    Criando segundos

--- RELOGIO1/script.js
+++ RELOGIO1/script.js
@@ -2,12 +2,14 @@
 
 var hour = document.querySelector('#hour')
 var minutes = document.querySelector('#minutes')
-var hour = document.querySelector('#hour')
+var seconds = document.querySelector('#seconds')
 
 var agora = new Date()
 
 var hora = agora.getHours() 
 var minutos = agora.getMinutes()
-
+var segundos = agora.getSeconds()
+ 
 hour.textContent = hora
 minutes.textContent = minutos
+seconds.textContent = segundos
