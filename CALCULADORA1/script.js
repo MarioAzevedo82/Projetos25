ede1ea102c562b9d33b5504694a9fd79f0b39746
Mario Azevedo <mariobct@msn.com>
2025-05-13 19:06:13 -0300

    função calculate

--- CALCULADORA1/script.js
+++ CALCULADORA1/script.js
@@ -12,3 +12,17 @@ function appendOperator(operator) {
     currentInput += operator
     display.textContent = currentInput
 }
+
+function calculate(){
+    try {
+        let result = eval(currentInput)
+        if(!Number.isInteger(result)){
+            result = result.toFixed(2)
+        }
+        currentInput = result
+        display.textContent = currentInput
+    } catch(error){
+        display.textContent = "Erro"
+        currentInput = ""
+    }
+}
