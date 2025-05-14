3e48203016a5c0e863fbe9e69cb39df170d4d550
Mario Azevedo <mariobct@msn.com>
2025-05-14 18:21:57 -0300

    js do relogio

--- CALCULADORA/CALCULADORA/script.js
+++ CALCULADORA/CALCULADORA/script.js
@@ -21,3 +21,21 @@ function calcular(){
         document.getElementById('resultado').innerHTML = "Erro"
     }
 }
+
+function horaAtual(){
+
+    var display = document.querySelector('.display')
+    var agora = new Date()
+    var horario = horaCerta(agora.getHours()) + ":" + horaCerta(agora.getMinutes()) + ":" + horaCerta(agora.getSeconds())
+    display.textContent = horario
+
+    function horaCerta(num){
+        if(num < 10){
+            num = "0" + num
+        } 
+        return num
+    }
+}
+
+setInterval(horaAtual, 1000)
+horaAtual()
