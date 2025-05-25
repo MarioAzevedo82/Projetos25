1b1004491b9398cb2d61d1d82cc1492d63bdcbc0
Mario Azevedo <mariobct@msn.com>
2025-05-25 10:16:22 -0300

    Cronômetro: start, pause e stop

--- TRIO/script.js
+++ TRIO/script.js
@@ -28,17 +28,30 @@ var hrs = 0
 var interval
 
 function start() {
-    alert('clicou')
+    watch()
+    interval = setInterval(watch, 1000)
 }
 
-function pause() {
-
+function pause() {  
+    clearInterval(interval)
 }
 
 function stop() {
-
+    clearInterval(interval)
+    sec = 0
+    min = 0
+    hrs = 0
+    document.getElementById('watch').innerHTML = "00:00:00"
 }
 
 function watch() {
-
+    sec++
+    if(sec == 60) {
+        min++
+        sec = 0
+    } if(min == 60){
+        hrs++
+        min = 0
+    }
+    document.getElementById('watch').innerHTML = (hrs) + ":" + (min) + ":" + (sec)
 }
