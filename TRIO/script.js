831107dc3ed8cd3ccc9a88cc9d82e2139df21671
Mario Azevedo <mariobct@msn.com>
2025-05-25 10:27:29 -0300

    Calculadora: insert, clean, back

--- TRIO/script.js
+++ TRIO/script.js
@@ -63,3 +63,20 @@ function watch() {
     }
     document.getElementById('watch').innerHTML = twoDigits(hrs) + ":" + twoDigits(min) + ":" + twoDigits(sec)
 }
+
+
+// CALCULADORA
+
+function insert(num) {
+    var numero = document.getElementById('visor').innerHTML
+    document.getElementById('visor').innerHTML = numero + num
+}
+
+function clean() {
+    document.getElementById('visor').innerHTML = ""
+}
+
+function back() {
+    var visor = document.getElementById('visor').innerHTML
+    document.getElementById('visor').innerHTML = visor.substring(0, visor.length - 1)
+}
