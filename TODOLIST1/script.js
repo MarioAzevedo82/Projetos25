02a8308fe5df0eff22b8f7811e9c8346279dcaed
Mario Azevedo <mariobct@msn.com>
2025-05-26 12:02:29 -0300

    js json

--- TODOLIST1/script.js
+++ TODOLIST1/script.js
@@ -1,8 +1,21 @@
+const localStorageKey = 'to-do-list-ma'
+
 function newTask() {
     let input = document.getElementById('input-new-task')
 
     //validation
     if(!input.value){
         alert('digite algo para inserir na lista')
+    }
+    // else if()
+    else
+    {
+        // increment to localStorage
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+        values.push({
+            name: input.value
+        })
+        localStorage.setItem(localStorageKey,JSON.stringify(values))
+        showValues()
     }
 }
