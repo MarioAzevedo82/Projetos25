cf1085a59733261072f8aafa09b2d357531e593a
Mario Azevedo <mariobct@msn.com>
2025-05-26 11:21:56 -0300

    js newTask() e validação

---
+++ TODOLIST1/script.js
@@ -0,0 +1,8 @@
+function newTask() {
+    let input = document.getElementById('input-new-task')
+
+    //validation
+    if(!input.value){
+        alert('digite algo para inserir na lista')
+    }
+}
