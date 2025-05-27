5d84b3fec8863514cf55c884bdb09c99bef6c264
Mario Azevedo <mariobct@msn.com>
2025-05-27 10:25:49 -0300

    JS showValues() ajustes

--- TODOLIST1/script.js
+++ TODOLIST1/script.js
@@ -25,6 +25,8 @@ function showValues(){
     let list = document.getElementById('to-do-list')
     list.innerHTML = ""
     for(let i = 0; i < values.length; i++){
-        list.innerHTML = `<li>${values[i]['name']}</li`
+        list.innerHTML += `<li>${values[i]['name']}</li>`
     }
 }
+
+showValues()
