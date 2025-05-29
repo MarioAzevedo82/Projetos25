25963ce92c9dfb80b66f661e6364e5d3adc758a9
Mario Azevedo <mariobct@msn.com>
2025-05-29 19:17:37 -0300

    tdlist4 const

--- TODOLIST4/script.js
+++ TODOLIST4/script.js
@@ -1,3 +1,5 @@
-function teste() {
-    alert('teste')
-}
+const input = document.getElementById('todo-input')
+const button = document.getElementById('add-btn')
+const list = document.getElementById('todo-list')
+
+
