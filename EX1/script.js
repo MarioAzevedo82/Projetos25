1af12eb0d288bc1e70daf3504d3e4f2f39227992
Mario Azevedo <mariobct@msn.com>
2025-06-10 12:11:11 -0300

    html calculadora

--- EX1/script.js
+++ EX1/script.js
@@ -107,3 +107,7 @@ function insert(num){
     var numero = document.getElementById('visor').innerHTML
     document.getElementById('visor').innerHTML = numero + num
 }
+
+function clean(){
+    document.getElementById('visor').innerHTML = ""
+}
