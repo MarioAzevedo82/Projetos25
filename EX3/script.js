8734ff526d4edba56d2c2eb587bd52d0533a94b1
Mario Azevedo <mariobct@msn.com>
2025-06-13 18:59:45 -0300

    setInterval

--- EX3/script.js
+++ EX3/script.js
@@ -1,12 +1,19 @@
 
 
 function ativarContagem(){
-    document.getElementById('tempo').innerHTML = "Começou a contagem!"
+    // document.getElementById('tempo').innerHTML = "Começou a contagem!"
     
-    tempo = setTimeout(function(){
-        document.getElementById('tempo').innerHTML = "Executou o setTimeout!"
-    }, 5000)
+    // tempo = setTimeout(function(){
+    //     document.getElementById('tempo').innerHTML = "Executou o setTimeout!"
+    // }, 5000)
+    setInterval(function() {
+        var cronometro = document.getElementById('tempo').innerHTML;
+        var soma = parseInt(cronometro) + 1;
+        document.getElementById('tempo').innerHTML = soma
+    },1000)
+
 }
+
 function pararContagem(){
     clearTimeout(tempo)
     document.getElementById('tempo').innerHTML = "Parou a contagem!"
