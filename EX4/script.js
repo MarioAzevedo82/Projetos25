36342c2ac39611710f173d28fddbb6ea3a0b3053
Mario Azevedo <mariobct@msn.com>
2025-06-14 11:34:24 -0300

    Criado intervalo

--- EX4/script.js
+++ EX4/script.js
@@ -3,8 +3,13 @@ function iniciarContagem() {
     const display = document.getElementById('relogio')
     display.textContent = tempo
 
-    setInterval(() => {
+    const intervalo = setInterval(() => {
         tempo--;
         document.getElementById('relogio').textContent = tempo
+        
+        if(tempo <= 0){
+            clearInterval(intervalo)
+            document.getElementById('relogio').textContent = "Contagem encerrada!"
+        } 
     }, 1000);
 }
