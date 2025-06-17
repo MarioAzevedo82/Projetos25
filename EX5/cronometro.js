34c75cb7d0f01d94e921e345dd5a1b1ef589305d
Mario Azevedo <mariobct@msn.com>
2025-06-17 19:17:30 -0300

    Função pausar

--- EX5/cronometro.js
+++ EX5/cronometro.js
@@ -8,5 +8,25 @@ function atualizarDisplay(){
     const tempo = document.getElementById('tempo');
     const min = minutos < 10? `0${minutos}`: minutos;
     const seg = segundos < 10? `0${segundos}`: segundos;
-    const tempo = `${min}:${seg}`
+    tempo.textContent = `${min}:${seg}`
 }
+
+function iniciar(){
+    if(!rodando){
+        rodando = true;
+        intervalo = setInterval(() => {
+            segundos++
+            if(segundos === 60){
+                segundos = 0
+                minutos++
+            }
+            atualizarDisplay()
+        }, 1000);
+    }
+}
+
+function pausar(){
+    clearInterval(intervalo)
+    rodando = false;
+}
+
