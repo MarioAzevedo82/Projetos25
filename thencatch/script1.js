303e7a05dcdd083dc005bd0c83643f9eb91c2e9f
Mario Azevedo <mariobct@msn.com>
2025-06-17 21:46:23 -0300

    simular API

--- thencatch/script1.js
+++ thencatch/script1.js
@@ -12,3 +12,29 @@ function carregarDados() {
         res.style.color = "green"
     })
 }
+
+// simulador API
+
+function simularAPI(){
+    const mensagem = document.getElementById("mensagem")
+    mensagem.textContent = "⏳ Carregando..."
+
+    new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            const sucesso = Math.random() > 0.5
+            if(sucesso){
+                resolve("Requisição bem sucedida!")
+            } else {
+                reject("Requisição falhou!")
+            }
+        }, 2000);
+    })
+    .then((msg)=>{
+        mensagem.textContent = msg
+        mensagem.style.color = "blue"
+    })
+    .catch((erro)=>{
+        mensagem.textContent = erro
+        mensagem.style.color = "red"
+    })
+}
