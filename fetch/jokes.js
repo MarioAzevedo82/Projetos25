85413fb176b09b7c99fedff33485ccba88957755
Mario Azevedo <mariobct@msn.com>
2025-06-23 12:47:18 -0300

    js piadas

--- fetch/jokes.js
+++ fetch/jokes.js
@@ -1,1 +1,17 @@
+function buscarPiada() {
+    const p = document.getElementById("piada")
+    p.textContent = "⏳ Buscando piada..."
 
+    fetch("https://api.chucknorris.io/jokes/random")
+        .then(res => {
+            if(!res.ok) throw new Error("❌ Não foi possível carregar a piada.")
+            return res.json()
+        })
+        .then(post => {
+            p.textContent = post.value
+        })
+        .catch(erro => {
+            p.textContent = "❌ Erro" + erro.message
+            p.style.color = "red"
+        })
+}
