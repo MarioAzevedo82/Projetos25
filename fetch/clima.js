4efc0c6d0ccdb69deb52fd442ab022cb2ea3e86d
Mario Azevedo <mariobct@msn.com>
2025-06-26 19:06:16 -0300

    .then dados

--- fetch/clima.js
+++ fetch/clima.js
@@ -2,11 +2,14 @@ function buscarClima(){
     const p = document.getElementById("clima")
     p.textContent = "⏳ Buscando clima..."
 
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=-23.55&longitude=-46.63&current_weather=true
-")
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=-23.55&longitude=-46.63&current_weather=true")
         .then(res =>{
             if(!res.ok) throw new Error("Erro ao buscar o clima")
             return res.json()
         })
-        
+        .theb(dados =>{
+            const temperatura = dados.current_weather.temperature
+            const vento = dados.current_weather.windspeed
+            p.textContent = `🌡 Temperatura: ${temperatura}°C | 💨 Vento: ${vento} Km/h`
+        })
 }
