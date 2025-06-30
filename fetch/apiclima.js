b6e13183900006001f9d005ad47c610539090f36
Mario Azevedo <mariobct@msn.com>
2025-06-29 21:08:47 -0300

    cityName

--- fetch/apiclima.js
+++ fetch/apiclima.js
@@ -1,3 +1,9 @@
 document.querySelector("#search").addEventListener('submit', (event) => {
     event.preventDefault()
+
+    const cityName = document.querySelector("#city_name").value
+
+    if (!cityName) {
+        return alert("Você precisa digitar uma cidade...")
+    }
 })
