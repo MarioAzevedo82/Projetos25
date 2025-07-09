a7784a3558fcd3bc239d90791c8be0b5149a5962
Mario Azevedo <mariobct@msn.com>
2025-07-09 19:16:51 -0300

    saudacao()

---
+++ EX7sets/exercicio1.js
@@ -0,0 +1,7 @@
+function saudacao() {
+    document.getElementById("saudacao").textContent = "⏳ Aguarde a saudação..."
+
+    setTimeout(() => {
+        document.getElementById("saudacao").textContent = "Olá, usuário!"
+    }, 2000);
+}
