8f6b5fd38aa312bd9d629cc073a655f7c616520f
Mario Azevedo <mariobct@msn.com>
2025-07-09 19:56:25 -0300

    dados()

--- EX7sets/exercicio1.js
+++ EX7sets/exercicio1.js
@@ -6,3 +6,11 @@ function saudacao() {
         document.getElementById("saudacao").textContent = "Olá, usuário!"
     }, 2000);
 }
+// Exercício 2
+function dados() {
+    document.getElementById("dados").textContent = "⏳ Carregando dados ..."
+
+    setTimeout(() => {
+        document.getElementById("dados").textContent = "✅ Dados carregados com sucesso!"
+    }, 4000);
+}
