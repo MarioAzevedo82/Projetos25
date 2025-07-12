3aeab9740d8296c515ab71c0b22b4d8c16c2c156
Mario Azevedo <mariobct@msn.com>
2025-07-12 11:55:28 -0300

    new Promisse

--- thencatch/exemplo1.js
+++ thencatch/exemplo1.js
@@ -1,4 +1,17 @@
 function buscar() {
     const mensagem = document.getElementById("mensagem")
     mensagem.textContent = "⏳ Buscando Dados ..."
+
+    new Promise((resolve, reject) => {
+        const sucesso = Math.random() > 0.5
+
+        setTimeout(() => {
+            if(sucesso) {
+                resolve("✅ Dados carregados com sucesso!")
+            } else {
+                reject("❌ Falha ao carregar Dados!")
+            }
+        }, 4000);
+    })
+
 }
