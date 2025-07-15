bf5ad5a6b443b0d63ad88538c2991032aaa8b39d
Mario Azevedo <mariobct@msn.com>
2025-07-15 08:59:28 -0300

    cores

--- thencatch/exemplo4.js
+++ thencatch/exemplo4.js
@@ -1,6 +1,7 @@
 function buscarMensagem() {
     const mensagem = document.getElementById("mensagem")
     mensagem.textContent = "⏳ Buscando mensagem ..."
+    mensagem.style.color = "black"
 
     new Promise((resolve, reject) => {
         const sucesso = Math.random() > 0.5
@@ -16,8 +17,10 @@ function buscarMensagem() {
     
     .then(msg => {
         mensagem.textContent = msg
+        mensagem.style.color = "green"
     })
     .catch(erro => {
         mensagem.textContent = erro
+        mensagem.style.color = "red"
     })
 }
