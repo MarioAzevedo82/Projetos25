2d979337b3e65cebda0c9f19327fee6fdcfed108
Mario Azevedo <mariobct@msn.com>
2025-07-15 08:51:50 -0300

    Promisse

--- thencatch/exemplo4.js
+++ thencatch/exemplo4.js
@@ -1,4 +1,16 @@
 function buscarMensagem() {
     const mensagem = document.getElementById("mensagem")
     mensagem.textContent = "⏳ Buscando mensagem ..."
+
+    new Promise((resolve, reject) => {
+        const sucesso = Math.random() > 0.5
+
+        setTimeout(() => {
+            if(sucesso){
+                resolve("✅ Mensagem encontrada com sucesso!")
+            }    else {
+                reject("❌ Erro ao encontrar a mensagem.")
+            }
+        }, 2000);
+    })
 }
