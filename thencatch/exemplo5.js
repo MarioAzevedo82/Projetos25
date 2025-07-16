98023950e597bdf2e62fd2628b05e9c00f463fe2
Mario Azevedo <mariobct@msn.com>
2025-07-16 18:31:49 -0300

    js e css

--- thencatch/exemplo5.js
+++ thencatch/exemplo5.js
@@ -2,6 +2,23 @@ function enviar() {
     const mensagem = document.getElementById("mensagem")
     const load = document.getElementById("load")
 
+    load.style.width = "0%"
     mensagem.textContent = "⏳ Aguarde para carregar..."
-    load.textContent = ""
+
+    let progresso = 0;
+
+    const intervalo = setInterval(() => {
+        progresso++
+        load.style.width = progresso + "%"
+
+        if(progresso >= 100) {
+            clearInterval(intervalo);
+
+            new Promise((resolve) => {
+                resolve("✅ Carregamento completo!")
+            }) .then(msg => {
+                mensagem.textContent =msg
+            })
+        }
+    }, 30);
 }
