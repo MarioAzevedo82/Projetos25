e660b785ab8e7f742ef6e69de3de873a17395f01
Mario Azevedo <mariobct@msn.com>
2025-07-23 20:44:42 -0300

    informacaoPaises assincronismo

--- asyncawait/main.js
+++ asyncawait/main.js
@@ -1,4 +1,14 @@
-console.log(1)
-console.log(2)
-setTimeout(() => console.log(3), 3000)
-console.log(4)
+// console.log(1)
+// console.log(2)
+// setTimeout(() => console.log(3), 3000)
+// console.log(4)
+
+async function informacaoPaises(paisDesejado){
+    const info = await fetch(
+        "https://restcountries.com/v3.1/name/" + paisDesejado
+    );
+    console.log(await info.json())
+}
+
+informacaoPaises("brazil")
+
