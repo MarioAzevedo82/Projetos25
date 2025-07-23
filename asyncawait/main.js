e66c39b819b5d2cd7a73ebfe82582f344f923142
Mario Azevedo <mariobct@msn.com>
2025-07-23 20:58:11 -0300

    informacaoPaises assincronismo gpt

--- asyncawait/main.js
+++ asyncawait/main.js
@@ -3,12 +3,22 @@
 // setTimeout(() => console.log(3), 3000)
 // console.log(4)
 
-async function informacaoPaises(paisDesejado){
-    const info = await fetch(
-        "https://restcountries.com/v3.1/name/" + paisDesejado
+// async function informacaoPaises(paisDesejado){
+//     const info = await fetch(
+//         "https://restcountries.com/v3.1/name/" + paisDesejado
+//     );
+//     console.log(await info.json())
+// }
+
+// informacaoPaises("brazil")
+
+async function informacaoPaises(paisDesejado) {
+    const resposta = await fetch(
+        `https://restcountries.com/v3.1/name/${paisDesejado}`
     );
-    console.log(await info.json())
+
+    if(!resposta.ok){
+        console.error("Erro ao buscar o país");
+        return;
+    }
 }
-
-informacaoPaises("brazil")
-
