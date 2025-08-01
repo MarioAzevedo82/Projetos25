aad854d571e85b991da535c41a15fd33ece1a3d4
Mario Azevedo <mariobct@msn.com>
2025-08-01 12:18:13 -0300

    const promessa

--- ASSINCRONA/PROMISE/desafio.js
+++ ASSINCRONA/PROMISE/desafio.js
@@ -2,10 +2,22 @@ function enviar() {
     const login = document.getElementById("login").value;
     const mensagem = document.getElementById("mensagem");
 
-    mensagem.textContent = "⏳ Carregando..."
+    mensagem.textContent = "⏳ Carregando...";
 
     if(login === ""){
-        alert("Por favor, digite seu login.")
-        return
+        alert("Por favor, digite seu login.");
+        return;
     }
+
+    const promessa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(login === admin || login === mario || login === ana || login === joao){
+                resolve("✅ Conectado!");
+            } else {
+                reject("❌ Usuário não cadastrado");
+            }
+        }, 2000);
+    })
+
+    
 }
