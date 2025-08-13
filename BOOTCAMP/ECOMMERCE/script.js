c9a4e2b9b632213a75d6328ac1683ef1915d0f44
Mario Azevedo <mariobct@msn.com>
2025-08-13 06:36:42 -0300

    Arquivos loja bolo

--- BOOTCAMP/ECOMMERCE/script.js
+++ BOOTCAMP/ECOMMERCE/script.js
@@ -112,6 +112,7 @@ function trocarCategoria() {
 
 window.addEventListener('DOMContentLoaded', function() {
     mostrarProdutos()
+    
     input.addEventListener("input", pesquisar)
 
     todosBotoes.forEach(botao => {
